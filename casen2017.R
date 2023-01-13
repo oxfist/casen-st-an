efff57a8667844a2c8932ad676a15ae08f5d7558
Andrés Quilodrán <andres.caro.q@gmail.com>
2023-01-13 01:03:04 -0300

    feat: filter rows with frequency = 0 from kable tables

--- casen2017.R
+++ casen2017.R
@@ -16,42 +16,39 @@ prepare_profesionales_ciencia <- function(data) {
 
 add_cv_variables <- function(data) {
   # Variables de SPSS que tienen valor y etiqueta para usar más fácilmente
-  area_labels <- as_factor(data$e7_cod_area, levels = "label")
-  subarea_labels <- as_factor(data$e7_subarea, levels = "label")
-  region_labels <- as_factor(data$region, levels = "label")
+  area_ocupacion_labels <- haven::as_factor(data$e7_cod_area, levels = "label")
+  subarea_ocupacion_labels <- haven::as_factor(data$e7_subarea, levels = "label")
+  region_labels <- haven::as_factor(data$region, levels = "label")
 
-  data %>% mutate(ocupacion = area_labels, subarea_ocupacion = subarea_labels, region_name = region_labels)
+  data %>% mutate(ocupacion = area_ocupacion_labels, subarea_ocupacion = subarea_ocupacion_labels, region_name = region_labels)
 }
 
 run_analyses <- function(data_ciencia) {
-  dsn <- svydesign(id = ~data_ciencia$folio, weights = ~data_ciencia$expr, data = data_ciencia)
-  freq_area_region <- svytable(~data_ciencia$ocupacion + data_ciencia$region_name, dsn) %>% addmargins()
-  freq_subarea_region <- svytable(~data_ciencia$subarea_ocupacion + data_ciencia$region_name, dsn) %>% addmargins()
+  dsn <- svydesign(id = ~ data_ciencia$folio, weights = ~ data_ciencia$expr, data = data_ciencia)
 
-  kable_area_region <- freq_area_region %>% kbl() %>%
+  # Generar tablas de frecuencia
+  freq_area_region <- svytable(~ data_ciencia$ocupacion + data_ciencia$region_name, dsn)
+  freq_subarea_region <- svytable(~ data_ciencia$subarea_ocupacion + data_ciencia$region_name, dsn)
+
+  kable_area_region <- freq_area_region[6:8, ] %>%
+    addmargins() %>%
+    kbl() %>%
     kable_styling(bootstrap_options = c("striped", "hover", "condensed", "responsive"))
-  kable_subarea_region <- freq_subarea_region %>% kbl() %>%
+  kable_subarea_region <- freq_subarea_region[c(12:17, 19), ] %>%
+    addmargins() %>%
+    kbl() %>%
     kable_styling(bootstrap_options = c("striped", "hover", "condensed", "responsive"))
 
   print(kable_area_region)
   print(kable_subarea_region)
 }
 
-data <- haven::read_sav('Casen 2017.sav')
+# 1. Leer archivo
+data <- haven::read_sav("Casen 2017.sav")
+
+# 2. Agregar variables necesarias
 data_ciencia <- prepare_profesionales_ciencia(data)
 data_ciencia <- add_cv_variables(data_ciencia)
 
+# 3. Generar tablas de frequencia
 run_analyses(data_ciencia)
-
-# get_cruce_area_ocupacion <- function(data) {
-#   crosstable(data$ocupacion ~ data$region_name, data = data, total = "both")
-# }
-#
-# cruce_area_ocupacion <- get_cruce_area_ocupacion(data_ciencia)
-# cruce_table <- cruce_area_ocupacion %>%
-#   select(!(.id:label)) %>%
-#   gt() %>%
-#   cols_label(variable = 'Área de Ocupación') %>%
-#   cols_width(everything() ~ px(120))
-#
-# print(cruce_table)
